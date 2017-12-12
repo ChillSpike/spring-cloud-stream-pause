377a1163b56ff8320753cdff53016b51daa7ab04
Vladislav Fefelov <vladislavf@playtika.com>
2017-12-12 12:39:03 +0300

    Remove unused modules

--- demo/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ demo/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
 @Slf4j
 @SpringBootApplication
 public class ConfigClientApplication {
